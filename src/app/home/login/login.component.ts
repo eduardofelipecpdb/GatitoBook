4a020395469ef107995725ab0d26f91e4a7939b7
eduardofelipecpdb <eduardofelipecpdb@gmail.com>
2022-09-21 18:44:38 -0300

    Commit parcial aula 2
    Commit parcial aula 2

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,33 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(
+    private authservice: AutenticacaoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authservice.autentica(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso');
+        this.router.navigate(['animais']);
+      },
+      (error) => {
+        alert('Usuário ou senha inválidos');
+        console.log(error);
+      }
+    );
   }
-
 }
